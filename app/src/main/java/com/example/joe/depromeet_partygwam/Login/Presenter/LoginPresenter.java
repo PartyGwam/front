6a4eec06032686fa0ec41173245e96b6a68a0162
whtjdhks3837 <qowod77@naver.com>
2018-06-26 20:06:01 +0900

    login / join..

--- app/src/main/java/com/example/joe/depromeet_partygwam/Login/Presenter/LoginPresenter.java
+++ app/src/main/java/com/example/joe/depromeet_partygwam/Login/Presenter/LoginPresenter.java
@@ -1,4 +1,41 @@
 package com.example.joe.depromeet_partygwam.Login.Presenter;
 
-public class LoginPresenter {
+import com.example.joe.depromeet_partygwam.Data.User;
+import com.example.joe.depromeet_partygwam.Login.Model.LoginRetrofitModel;
+import com.example.joe.depromeet_partygwam.Login.Model.LoginModelCallback;
+
+public class Presenter implements LoginContract.Presenter, LoginModelCallback.RetrofitCallback {
+
+    private LoginContract.View view;
+    private LoginModelCallback.RetrofitCallback retrofitCallback;
+    private LoginRetrofitModel retrofitModel;
+
+    public Presenter() {
+
+    }
+
+    @Override
+    public void onSuccess(int code) {
+
+    }
+
+    @Override
+    public void onFailure(int code) {
+
+    }
+
+    @Override
+    public void attachView(LoginContract.View view) {
+
+    }
+
+    @Override
+    public void detachView() {
+
+    }
+
+    @Override
+    public void validateUser(User user) {
+
+    }
 }
