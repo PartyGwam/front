94e05c3703cd94ba063973c7f80a1ea6f5b50a52
Munkyung Choi <munkyung0324@gmail.com>
2018-07-09 21:04:49 +0900

    파티 디테일 화면 작업중

--- app/src/main/java/com/example/joe/depromeet_partygwam/PartyDetail/View/PartyDetailActivity.java
+++ app/src/main/java/com/example/joe/depromeet_partygwam/PartyDetail/View/PartyDetailActivity.java
@@ -2,9 +2,11 @@ package com.example.joe.depromeet_partygwam.PartyDetail.View;
 
 import android.content.Intent;
 import android.os.Parcelable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -46,12 +48,8 @@ public class PartyDetailActivity extends AppCompatActivity
     TextView partyContent;
     @BindView (R.id.party_leader_image)
     ImageView partyLeaderImage;
-    @BindView(R.id.party_leader_nickname)
-    TextView partyLeaderNick;
-    @BindView(R.id.party_create_time)
-    TextView partyCreateTime;
-    @BindView(R.id.view_count)
-    TextView viewCnt;
+    @BindView(R.id.party_info)
+    TextView partyInfo;
     @BindView(R.id.join_button)
     ImageView joinBtn;
     @BindView(R.id.num_of_reply)
@@ -82,18 +80,28 @@ public class PartyDetailActivity extends AppCompatActivity
     }
 
     public void onBindView(){
-        partyTitle.setText(data.getTitle());
-        partyPlace.setText(data.getPlace());
         String startDate = data.getStartTime().split("T")[0];
         String startTime = data.getStartTime().split("T")[1].substring(0, 5);
+        String createTime = data.getCreatedAt().split("T")[1].substring(0, 5);
+        String info = data.getPartyOwner().getUsername() + " | "
+                + createTime + " | 조회수 ";
+
+        if (data.getTitle().length() > 13) {
+            partyTitle.setText(data.getTitle().substring(0, 13) + "...");
+        } else {
+            partyTitle.setText(data.getTitle());
+        }
+
         if (startDate.equals(date)) {
             partyDate.setText("오늘 " + startTime);
         }else {
-            partyDate.setText(startDate.split("-")[1] + "." + startDate.split("-")[2]);
+            partyDate.setText(startDate.split("-")[1] + "." + startDate.split("-")[2] + "  " + startTime);
         }
-        partyDate.setText(data.getStartTime());
-        //joinNumber.setText(data.getCurrentPeople());
-        //maxNumber.setText(data.getMaxPeople());
+
+        partyInfo.setText(info);
+        partyPlace.setText(data.getPlace());
+        joinNumber.setText(data.getCurrentPeople() + "");
+        maxNumber.setText(data.getMaxPeople() + "");
         partyContent.setText(data.getDescription());
     }
 
@@ -112,14 +120,22 @@ public class PartyDetailActivity extends AppCompatActivity
 
     @OnClick(R.id.edit_party)
     public void editBtnClick(){
-        //글 수정/글 삭제 팝업? 띄우기
         Intent intent = new Intent(getApplicationContext(), PartyEditPopupActivity.class);
-        startActivity(intent);
+        intent.putExtra("item", data);
+        startActivityForResult(intent, 101);
+    }
+
+    @OnClick(R.id.join_button)
+    public void joinBtnClick(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(R.layout.join_party_btn_dialog_alarm);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
     private void updateParty(){
         //파티에 수정된 게 있으면 서버로 보내준다음
         //PartyListFragment 로 돌아가기
-
     }
 }
